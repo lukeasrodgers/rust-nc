1a04de46ef3d52bb632f19838b2bc5a1a491862a
Luke Rodgers <lukeasrodgers@gmail.com>
2015-02-16 09:10:48 -0600

    connecting sorta works

--- src/connecter.rs
+++ src/connecter.rs
@@ -1,4 +1,21 @@
 use getopts::{Options,Matches};
+use std::net::{TcpStream,SocketAddr};
 
 pub fn connect(matches: &Matches) {
+    let host = matches.free.get(0).unwrap();
+    let port = match matches.free.get(1).unwrap().parse::<u16>() {
+        Ok(n) => { n }
+        Err(_) => {
+            panic!("not in possible range".to_string());
+        }
+    };
+    // let connect_addr = SocketAddr::new(host, port);
+    match TcpStream::connect(&(host.as_slice(), port)) {
+        Ok(stream) => {
+            println!("ok");
+        },
+        Err(f) => {
+            // just exit
+        }
+    }
 }
