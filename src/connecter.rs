84df50715882edab8e459bec217b74db2960337a
Luke Rodgers <lukeasrodgers@gmail.com>
2015-03-08 23:32:33 -0400

    getting rid of unused `readwrite` fn

--- src/connecter.rs
+++ src/connecter.rs
@@ -108,31 +108,6 @@ fn readwrite_chan(channel: &EventLoopSender<String>) {
     }
 }
 
-fn readwrite(stream: &TcpStream) {
-    let mut buf_stream = BufStream::new(stream);
-    let mut stdin_reader = old_io::stdin();
-    let mut read_buf = [0, 4096];
-    loop {
-        // Have to block here, so we can't immediately terminate if server closes socket.
-        match stdin_reader.read(&mut read_buf) {
-            Ok(n) => {
-                buf_stream.write_all(&read_buf.slice_to(n)).unwrap();
-                match buf_stream.flush() {
-                    Ok(_) => { /* */ },
-                    Err(f) => {
-                        println!("closed");
-                        // other end closed socket
-                        return;
-                    }
-                }
-            },
-            Err(f) => {
-                panic!(f.to_string());
-            }
-        }
-    }
-}
-
 // pretty ugly...
 fn parse_addr(matches: &Matches) -> SocketAddr {
     let host = matches.free.get(0).unwrap();
